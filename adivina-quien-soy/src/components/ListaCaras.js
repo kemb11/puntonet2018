9d9fd994145b44ae64312731d57c1a5707674311
Kevin <kevineloy11@gmail.com>
2018-10-24 23:21:20 -0300

    eliminado codigo comentado

--- adivina-quien-soy/src/components/ListaCaras.js
+++ adivina-quien-soy/src/components/ListaCaras.js
@@ -30,56 +30,14 @@ class ListaCaras extends Component {
         console.log("respuesta: "+JSON.stringify(responseJson));
 
         this.setState({personajes: responseJson.personajes, filtrados: responseJson.user1.filtrados, cargados: true});
-        
-        // Ya estaba en partida, cargar personajes de localstorage
-        /*if(responseJson.cantPreguntas){
-          alert("Ya esta en partida...");
-          var personajes = localStorage.getItem('listaPersonajes');
-          this.setState({personajes: personajes, cargados: true});
-        }else{
-          let personajes = [];
-
-          responseJson.map(personaje => { 
-            return(personajes.push(personaje));
-          });
-
-          localStorage.setItem('listaPersonajes', this.state);
-          this.setState({personajes: personajes, cargados: true});
-        }*/
+          window.localStorage.setItem('enPartida', true);
       });
     }
   }
 
   componentDidMount(){
-    /*const {socket} = this.props;
-    var thisAux = this;
-    socket.on("a_ocultar", function(data){
-      console.log("a_ocultar: "+data);
-      
-      if(Array.isArray(data)){
-        const personajes = [...thisAux.state.personajes];
-        for(var i=0; i<personajes.length;i++){
-          for(var z=0; z<data.length; z++){
-            if(personajes[i]._id === data[z]){
-              personajes[i].a_ocultar = true;
-            }
-          }
-        }
-        console.log(personajes);
-        thisAux.setState({personajes: personajes});
-      }
-      else{
-        const personajes = [...thisAux.state.personajes];
-        const personaje = personajes.find( p => p._id === data);
-        personaje.a_ocultar = true;
-        thisAux.setState({personajes: personajes});
-      }
-
-      // Actualizar lista de personajes en localstorage
-      localStorage.setItem('listaPersonajes', this.state);
-    });*/
+    
   }
-
 
   render() {
     const {socket} = this.props;
