14bf5c3615db67167865f0d2ec97a584c269f929
Nicolas Carignani <2bccarignani3@gmail.com>
2018-10-30 15:45:46 -0300

    ranking

--- adivina-quien-soy/src/components/Inicio.js
+++ adivina-quien-soy/src/components/Inicio.js
@@ -3,23 +3,31 @@ import './css/inicio.css';
 import Cabecera from './Cabecera';
 import Pie from './Pie';
 import {Redirect} from 'react-router-dom';
+import {GetApi} from '../servicios/GetApi';
 
 class Inicio extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {redirect: false, red_path: '/' };
+    this.state = {redirect: false, red_path: '/', ranking_cargado: false, ranking: []};
   }
 
   jugar = () =>{
     this.setState({red_path: '/partida', redirect: true});
   }
 
+  componentDidMount(){
+    if(!this.state.ranking_cargado){
+      GetApi('usuarios/ranking','').then((result) => {
+        this.setState({ranking_cargado: true,ranking: result.ranking_usuarios});
+      });
+    }
+  }
+
   render() {
     if(this.state.redirect){
       return <Redirect to={this.state.red_path} />;
     }
-
     return (
       <div className="Inicio">
         <Cabecera />
@@ -31,6 +39,28 @@ class Inicio extends Component {
             Con cada pregunta iras filtrando los personajes, aumentando asi tus posibilidades de acierto.</h3>
 
             <button className="boton" onClick={this.jugar}>Jugar</button>
+          </div>
+          <div className="ranking">
+            <table class="table table-hover table-dark">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Usuario</th>
+                  <th scope="col">Score</th>
+                </tr>
+              </thead>
+              <tbody>
+                {!this.state.ranking_cargado ? "..." : null}
+                {this.state.ranking.map((usuario,index) => {
+                  return usuario = 
+                        <tr>
+                          <th scope="row">{index}</th>
+                          <td>{usuario.nickname}</td>
+                          <td>{usuario.score}</td>
+                        </tr>
+                })}
+              </tbody>
+            </table>
           </div>
         </div>
         <Pie />
