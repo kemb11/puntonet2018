2ab372f1e0e32f17a4fde5d3487d08a29701ba12
stephiRM <srudenkomuller@gmail.com>
2018-11-07 19:58:02 -0300

    Ver Perfil

--- adivina-quien-soy/src/components/VerPerfil.js
+++ adivina-quien-soy/src/components/VerPerfil.js
@@ -2,33 +2,42 @@ import React, { Component } from 'react';
 import './css/verperfil.css';
 import Cabecera from './Cabecera';
 import {PostApi} from '../servicios/PostApi';
+import {GetApi} from '../servicios/GetApi';
 
 class VerPerfil extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {redirect: false};
+		this.state = {
+			redirect: false,
+			user: {nickname: "", email: ""}
+		};
 	}
 	
-	render() {
+	componentDidMount(){
+		GetApi('usuarios/me',localStorage.getItem("token")).then((result) => {
+			console.log(result);
+			if(result!=null){
+				this.setState({user: result.user});
+			}
+		});
+	}
+
+	render() {	
 		return (
 			<div className="VerPerfil">
-			<Cabecera />	
+				<Cabecera />	
 				<div className="container">
 					<div className="col col-6 text-center">
 						<center><h2>Ver Perfil del usuario</h2></center>
 					</div>	
-					
+						
 					<div classname="row">
 						<div>
-							<h4>Nickname: nick</h4>
+							<h4>Nickname: {this.state.user.nickname}</h4>
 						</div>
 						
 						<div>
-							<h4>Email: correo@correo.com</h4>
-						</div>
-						
-						<div>
-							<h4>Cambiar contraseña</h4>
+							<h4>Correo: {this.state.user.email}</h4>
 						</div>
 						
 						<div>
@@ -46,15 +55,11 @@ class VerPerfil extends Component {
 						<div>
 							<h4>Porcentaje de ganadas: 30%</h4>
 						</div>
-						
-						<div>
-							 <a className="navbar-brand" href="/"><h3>Volver</h3></a>
-						</div>
 					</div>
 				</div>
-			</div>
-		);
-  }
+			</div>		
+			);
+	}
 }
 
 export default VerPerfil;
