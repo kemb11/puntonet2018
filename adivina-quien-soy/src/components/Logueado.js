41329a6cf7c884554d3b975e1d1f86b31f2f9e6a
Nicolas Carignani <2bccarignani3@gmail.com>
2018-11-27 19:04:26 -0300

    mostrar nombre usuario

--- adivina-quien-soy/src/components/Logueado.js
+++ adivina-quien-soy/src/components/Logueado.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import './css/logueado.css';
 import iconoUsr from '../img/user.png';
 import {Redirect} from 'react-router-dom';
+import {GetApi} from '../servicios/GetApi';
 
 class Logueado extends Component {
   constructor(props) {
     super(props);
     this.state = {
       redirect: false,
-      path: "/"
+      path: "/",
+      user: ''
     };
   }
 
@@ -21,11 +23,21 @@ class Logueado extends Component {
 	  this.setState({redirect: true, path:'/verperfil'});
   }
 
+  componentDidMount(){
+		GetApi('usuarios/me',localStorage.getItem("token")).then((result) => {
+			console.log(result);
+			if(result!=null){
+				this.setState({user: result.user.nickname});
+			}
+		});
+	}
+  
   render() {
     var token = window.localStorage.getItem('token');
     if(this.state.redirect || token === 'undefined' || token === null){
       return <Redirect to={this.state.path} />;
     }
+
     return (
       <div className="Logueado">
         <div className="nav navbar-nav navbar-right text-center dropdown">
@@ -33,7 +45,7 @@ class Logueado extends Component {
             <img src={iconoUsr} alt="icono de usuario"/>
           </button>
           <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <h5 className="dropdown-item">nickname</h5>
+            <h5 className="dropdown-item">{this.state.user}</h5>
             <button onClick={this.perfil} className="dropdown-item">Ver Perfil</button>
             <button onClick={this.salir} className="dropdown-item">Salir</button>
           </div>
